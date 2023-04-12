0e70924ee17022fd2b12d1f5c1ec3e3a3bae838a
Reydem <reydem613@gmail.com>
2023-04-12 17:17:34 -0500

    Instalado redux-devtools de forma correcta

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'
-
+import store from './store';
+import { Provider } from 'react-redux';
 import Error404 from 'containers/errors/Error404';
 import Home from 'containers/pages/Home';
 function App(){
   return (
-   <Router>
-        <Routes>
-        {/* Error Display */}     
-            <Route path ="*" element={<Error404/>}/>
-   
-            {/* Home Display */}
-            <Route path="/" element={<Home/>}/>
+    <Provider store={store}>
+        <Router>
+            <Routes>
+              {/* Error Display */}     
+                <Route path ="*" element={<Error404/>}/>
+      
+                    {/* Home Display */}
+                <Route path="/" element={<Home/>}/>
             </Routes>
         </Router>
+    </Provider>
   );
 }
 export default App;

aafff0b32f6e403963fee671b82cbce5c99803e6
Reydem <reydem613@gmail.com>
2023-04-21 18:17:41 -0500

    Agredado menu de navegacion

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -2,12 +2,21 @@ import { connect } from 'react-redux'
 function Navbar() {
     return (
         <nav className='w-full py-10 shadow-md fixed'>
-            <div className="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
+            <div className="bg-white px-4 sm:px-6">
                 <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap">
                     <div className="ml-4 mt-2">
                         <h3 className="text-lg font-medium leading-6 text-gray-900">Job Postings</h3>
                     </div>
                     <div className="ml-4 mt-2 flex-shrink-0">
+                        <div className="ml-4 mt-2 flex-shrink-0">
+                            <h3 to='/casos' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Casos</h3>
+                            <h3 to='/servicios' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Servicios</h3>
+                            <h3 to='/nosotros' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Nosotros</h3>
+                            <h3 to='/carreras' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Carreras</h3>
+                            <h3 to='/blog' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Blog</h3>
+                            <h3 to='/contacto' className="text-lg inline-flex font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Contacto</h3>
+
+                        </div>
                         <button
                             type="button"
                             className="relative inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">

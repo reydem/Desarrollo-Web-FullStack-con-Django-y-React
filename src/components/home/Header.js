15b3c0557aec30e47e465cd88828e418d975a6ab
Reydem <reydem613@gmail.com>
2023-04-22 10:35:02 -0500

    Logrado Typewriter

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,3 +1,4 @@
+import { Typewriter } from 'react-simple-typewriter'
 import { Link } from 'react-router-dom'
 function Header() {
 
@@ -21,10 +22,21 @@ function Header() {
       <div className="mx-auto max-w-full xl:mx-12 xl:pt-40 xl:pb-64 lg:pt-40 lg:pb-48 pt-24 pb-12  ">
         <div>
           <div>
-            <h1 className="text-4xl font-semibold tracking-tight pb-16  sm:text-7xl">
-            Agency for <span> </span>
-
-            </h1>
+          <h1 className="text-4xl font-semibold tracking-tight pb-16  sm:text-7xl">
+              Agency for <span> </span>
+                  <Typewriter
+                        words={['Dreamers', 'Creators', 'You!']}
+                        loop={0}
+                        cursor
+                        
+                        cursorStyle='_'
+                        typeSpeed={120}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                        // onLoopDone={handleDone}
+                        // onType={handleType}
+                    />
+              </h1>
             <p className="mt-16 text-2xl max-w-3xl leading-8 text-black ">
             Jumpstart today your business with one of our services that range from <span className='lg:text-xl text-md font-medium leading-8 text-gray-700 sm:text-center  transition duration-300 ease-in-out border-b-2 border-transparent hover:border-orange-button'><Link to='/'>Web Development</Link></span> 
             <span className='mx-1 lg:text-xl text-md font-medium leading-8 text-gray-700 sm:text-center transition duration-300 ease-in-out border-b-2 border-transparent hover:border-orange-button'><Link to='/'> App Development</Link></span>

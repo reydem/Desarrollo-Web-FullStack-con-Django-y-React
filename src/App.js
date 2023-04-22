0e2e327b0e464c8646615c4b8a837ef63dcedc8f
Reydem <reydem613@gmail.com>
2023-04-21 20:46:13 -0500

    Logrado componer footer

--- src/App.js
+++ src/App.js
@@ -3,18 +3,38 @@ import store from './store';
 import { Provider } from 'react-redux';
 import Error404 from 'containers/errors/Error404';
 import Home from 'containers/pages/Home';
-function App(){
+
+
+
+
+
+import Cases from 'containers/pages/Cases';
+
+import Services from 'containers/pages/Services';
+import About from 'containers/pages/About';
+import Careers from 'containers/pages/Careers';
+import Blog from 'containers/pages/Blog';
+import Contact from 'containers/pages/Contact';
+
+// import { AnimatePresence } from 'framer-motion'
+function App() {
   return (
     <Provider store={store}>
-        <Router>
-            <Routes>
-              {/* Error Display */}     
-                <Route path ="*" element={<Error404/>}/>
-      
-                    {/* Home Display */}
-                <Route path="/" element={<Home/>}/>
-            </Routes>
-        </Router>
+      <Router>
+        <Routes>
+          {/* Error Display */}
+          <Route path="*" element={<Error404 />} />
+
+          {/* Home Display */}
+          <Route path="/" element={<Home />} />
+          <Route path="/casos" element={<Cases />} />
+          <Route path="/servicios" element={<Services />} />
+          <Route path="/nosotros" element={<About />} />
+          <Route path="/carreras" element={<Careers />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contacto" element={<Contact />} />
+        </Routes>
+      </Router>
     </Provider>
   );
 }

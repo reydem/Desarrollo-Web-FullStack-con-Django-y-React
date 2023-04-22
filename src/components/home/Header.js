f7c75154999bc77542bf4a7e16c9da02fe58f08d
Reydem <reydem613@gmail.com>
2023-04-22 12:54:20 -0500

    Componiendo de nuevo con el navbar

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -21,57 +21,53 @@ function Header() {
         <div className="relative px-6 lg:px-8">
           <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
             <div>
-              <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                  <span className="text-gray-600">
-                    Announcing our next round of funding.{' '}
-                    <a href="#" className="font-semibold text-indigo-600">
-                      <span className="absolute inset-0" aria-hidden="true" />
-                      Read more <span aria-hidden="true">&rarr;</span>
-                    </a>
-                  </span>
-                </div>
-              </div>
               <div>
                 <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
                 Conteo <span> </span>
+                {/* <div className='inline-flex' style={{color: '#FF561E', fontweight: 'bold'}}> */}
                   <Typewriter
-                        words={['1.....', '2.....', '3.....']}
-                        loop={0}
-                        cursor
-                        
-                        cursorStyle='_'
-                        typeSpeed={120}
-                        deleteSpeed={50}
-                        delaySpeed={1000}
-                        // onLoopDone={handleDone}
-                        // onType={handleType}
-                    />
+                  words={['1.....', '2.....', '3.....']}
+                  loop={0}
+                  cursor
+                  
+                  cursorStyle='_'
+                  typeSpeed={120}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                  // onLoopDone={handleDone}
+                  // onType={handleType}
+              />
+                {/* </div> */}
+
                 </h1>
-                <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
-                  amet fugiat veniam occaecat fugiat aliqua.
-                </p>
-                <div className="mt-8 flex gap-x-4 sm:justify-center">
-                  <a
-                    href="#"
-                    className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
-                  >
-                    Get started
-                    <span className="text-indigo-200" aria-hidden="true">
-                      &rarr;
-                    </span>
-                  </a>
-                  <a
-                    href="#"
-                    className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-                  >
-                    Live demo
-                    <span className="text-gray-400" aria-hidden="true">
-                      &rarr;
-                    </span>
-                  </a>
-                </div>
+                <ul className="flex gap-8 items-center justify-center py-12">
+                <li className="inline-flex border-b-2 border-transparent hover:border-orange-button transition duration-300 ease-in-out mx-4">
+                  <Link to="/servicos/AlgoAqui" className='mt-6 text-lg font-medium leading-8 text-gray-700 sm:text-center '>
+                    item 1
+                  </Link>
+                </li>
+                <li className="inline-flex border-b-2 border-transparent hover:border-orange-button transition duration-300 ease-in-out mx-4">
+                  <Link to="/servicos/AlgoAqui" className='mt-6 text-lg font-medium leading-8 text-gray-700 sm:text-center '>
+                    item 1
+                  </Link>
+                </li>
+                <li className="inline-flex border-b-2 border-transparent hover:border-orange-button transition duration-300 ease-in-out mx-4">
+                  <Link to="/servicos/AlgoAqui" className='mt-6 text-lg font-medium leading-8 text-gray-700 sm:text-center '>
+                    item 1
+                  </Link>
+                </li>
+                <li className="inline-flex border-b-2 border-transparent hover:border-orange-button transition duration-300 ease-in-out mx-4">
+                  <Link to="/servicos/AlgoAqui" className='mt-6 text-lg font-medium leading-8 text-gray-700 sm:text-center '>
+                    item 1
+                  </Link>
+                </li>
+                <li className="inline-flex border-b-2 border-transparent hover:border-orange-button transition duration-300 ease-in-out mx-4">
+                  <Link to="/servicos/AlgoAqui" className='mt-6 text-lg font-medium leading-8 text-gray-700 sm:text-center '>
+                    item 1
+                  </Link>
+                </li>
+              </ul>
+              
               </div>
               <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
                 <svg
--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -10,6 +10,20 @@ function Navbar() {
 
     const [loading, setLoading] = useState(true)
 
+    window.onscroll = function () { scrollFunction() }
+
+    function scrollFunction() {
+        if (document.getElementById('navbar')) {
+          if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            document.getElementById('navbar').classList.add('shadow-navbar');
+            document.getElementById('navbar').classList.add('bg-white');
+          } else {
+            document.getElementById('navbar').classList.remove('shadow-navbar');
+            document.getElementById('navbar').classList.remove('bg-white');
+          }
+        }
+      }
+
     return (
         <nav data-scroll data-scroll-id="hey" id='navbar' className='w-full py-6 top-0 transition duration-300 ease-in-out z-40 fixed'>
             <div className="px-4 sm:px-6">

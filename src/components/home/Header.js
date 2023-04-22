7a7599c8855d000745c05bdc57ec0325c865edf3
Reydem <reydem613@gmail.com>
2023-04-22 08:23:03 -0500

    agregando header

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,81 +1,37 @@
-
+import { Link } from 'react-router-dom'
 function Header() {
   return (
     <main>
-      <div className="relative px-6 lg:px-8">
-        <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+    <div className="relative px-6 lg:px-8">
+      <div className="mx-auto max-w-full xl:mx-12 xl:pt-40 xl:pb-64 lg:pt-40 lg:pb-48 pt-24 pb-12  ">
+        <div>
           <div>
-            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-              <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                <span className="text-gray-600">
-                  Announcing our next round of funding.{' '}
-                  <a href="#" className="font-semibold text-indigo-600">
-                    <span className="absolute inset-0" aria-hidden="true" />
-                    Read more <span aria-hidden="true">&rarr;</span>
-                  </a>
-                </span>
-              </div>
-            </div>
-            <div>
-              <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
-                Data to enrich your online business
-              </h1>
-              <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
-                amet fugiat veniam occaecat fugiat aliqua.
-              </p>
-              <div className="mt-8 flex gap-x-4 sm:justify-center">
-                <a
-                  href="#"
-                  className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
-                >
-                  Get started
-                  <span className="text-indigo-200" aria-hidden="true">
-                    &rarr;
-                  </span>
-                </a>
-                <a
-                  href="#"
-                  className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-                >
-                  Live demo
-                  <span className="text-gray-400" aria-hidden="true">
-                    &rarr;
-                  </span>
-                </a>
-              </div>
-            </div>
-            <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-              <svg
-                className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
-                viewBox="0 0 1155 678"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
-                  fillOpacity=".3"
-                  d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-                />
-                <defs>
-                  <linearGradient
-                    id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
-                    x1="1155.49"
-                    x2="-78.208"
-                    y1=".177"
-                    y2="474.645"
-                    gradientUnits="userSpaceOnUse"
-                  >
-                    <stop stopColor="#9089FC" />
-                    <stop offset={1} stopColor="#FF80B5" />
-                  </linearGradient>
-                </defs>
-              </svg>
-            </div>
+            <h1 className="text-4xl font-semibold tracking-tight pb-16  sm:text-7xl">
+            Agency for <span> </span>
+
+            </h1>
+            <p className="mt-16 text-2xl max-w-3xl leading-8 text-black ">
+            Jumpstart today your business with one of our services that range from <span className='lg:text-xl text-md font-medium leading-8 text-gray-700 sm:text-center  transition duration-300 ease-in-out border-b-2 border-transparent hover:border-orange-button'><Link to='/'>Web Development</Link></span> 
+            <span className='mx-1 lg:text-xl text-md font-medium leading-8 text-gray-700 sm:text-center transition duration-300 ease-in-out border-b-2 border-transparent hover:border-orange-button'><Link to='/'> App Development</Link></span>
+            <span className='mx-1 lg:text-xl text-md font-medium leading-8 text-gray-700 sm:text-center transition duration-300 ease-in-out border-b-2 border-transparent hover:border-orange-button'><Link to='/'> Game Development</Link></span>
+            <span className='mx-1 lg:text-xl text-md font-medium leading-8 text-gray-700 sm:text-center transition duration-300 ease-in-out border-b-2 border-transparent hover:border-orange-button'><Link to='/'> Digital Marketing</Link></span>
+            </p>
+            {/* <div className="absolute bottom-0 left-0 flex space-x-6 xl:mx-20 mx-6">
+            {navigation.social.map((item) => (
+              <a key={item.name} href={item.href} className="text-gray-900 hover:text-gray-800">
+                <span className="sr-only">{item.name}</span>
+                <item.icon className="h-8 w-8" aria-hidden="true" />
+              </a>
+            ))}
+            </div> */}
+          </div>
+          <div className="absolute inset-x-0 top-[calc(100%-20rem)] -z-10 transform-gpu overflow-hidden bg-white blur-lg lg:top-[calc(100%-45rem)] sm:top-[calc(100%-30rem)]">
+            <img src="https://bafybeicgamofiuvkc6wjxl4wwzzh6pdovhcvvyc2gw5verruiolnykzz3i.ipfs.w3s.link/bbub3.jpg" className='w-full h-full object-cover'/>
           </div>
         </div>
       </div>
-    </main>
+    </div>
+  </main>
   )
 }
 

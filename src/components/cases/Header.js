e70b5fed69c0845b00fdff4284770dc72b9ec5ea
Reydem <reydem613@gmail.com>
2023-04-22 16:15:15 -0500

    Adecuando pagina de casos de estudio

--- src/components/cases/Header.js
+++ src/components/cases/Header.js
@@ -4,7 +4,7 @@ function Header() {
 
   window.onscroll = function () { scrollFunction() }
 
-    function scrollFunction() {
+  function scrollFunction() {
     if (document.getElementById('navbar')) {
       if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById('navbar').classList.add('shadow-navbar');
@@ -18,15 +18,25 @@ function Header() {
 
   return (
     <main>
-        <div className="relative px-6 lg:px-8">
-          <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+      <div className="relative px-6 lg:px-8">
+        <div className="mx-auto max-w-full xl:mx-12 xl:pt-40 xl:pb-64 lg:pt-40 lg:pb-48 pt-24 pb-12">
+          <div>
+
+
+
             <div>
-              <div>
-                <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
+              <h1 className="text-4xl font-bold tracking-tight pb-16 sm:text-6xl">
                 Casos de estudios
 
-                </h1>
-                <ul className="flex gap-8 items-center justify-center py-12">
+              </h1>
+              <p className="mt-16 text-2xl max-w-3xl leading-8 text-black ">
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
+                amet fugiat veniam occaecat fugiat aliqua.
+              </p>
+
+
+
+              <ul className="flex gap-8 items-center justify-center py-12">
                 <li className="inline-flex border-b-2 border-transparent hover:border-orange-button transition duration-300 ease-in-out mx-4">
                   <Link to="/servicos/AlgoAqui" className='mt-6 text-lg font-medium leading-8 text-gray-700 sm:text-center '>
                     item 1
@@ -53,39 +63,21 @@ function Header() {
                   </Link>
                 </li>
               </ul>
-              
-              </div>
-              <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-                <svg
-                  className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
-                  viewBox="0 0 1155 678"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
-                    fillOpacity=".3"
-                    d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-                  />
-                  <defs>
-                    <linearGradient
-                      id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
-                      x1="1155.49"
-                      x2="-78.208"
-                      y1=".177"
-                      y2="474.645"
-                      gradientUnits="userSpaceOnUse"
-                    >
-                      <stop stopColor="#9089FC" />
-                      <stop offset={1} stopColor="#FF80B5" />
-                    </linearGradient>
-                  </defs>
-                </svg>
-              </div>
+
+
+
+
+            </div>
+
+
+
+            <div className="absolute inset-x-0 top-[calc(100%-20rem)] -z-10 transform-gpu overflow-hidden bg-white blur-lg lg:top-[calc(100%-45rem)] sm:top-[calc(100%-30rem)]">
+              <img src="https://bafybeicgamofiuvkc6wjxl4wwzzh6pdovhcvvyc2gw5verruiolnykzz3i.ipfs.w3s.link/bbub3.jpg" className='w-full h-full object-cover' />
             </div>
           </div>
         </div>
-      </main>
+      </div>
+    </main>
 
   )
 }

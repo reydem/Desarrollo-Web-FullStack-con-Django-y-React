840785dc8b982858eb603c954f5331f7922caf2b
Reydem <reydem613@gmail.com>
2023-04-22 10:20:37 -0500

    No se pudo instalar @heroicons/react/24/outline ni @headlessui/react

--- src/components/home/Features.js
+++ src/components/home/Features.js
@@ -34,6 +34,7 @@ export default function Features() {
               <div key={feature.name} className="relative">
                 <dt>
                   {/* <CheckIcon className="absolute h-6 w-6 text-green-500" aria-hidden="true" /> */}
+                  <div className="absolute h-6 w-6 text-green-500" aria-hidden="true" />
                   <p className="ml-9 text-lg font-medium leading-6 text-gray-900">{feature.name}</p>
                 </dt>
                 <dd className="mt-2 ml-9 text-base text-gray-500">{feature.description}</dd>

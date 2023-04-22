c2af93344c37c141d151a4133a4db0bd958fb00a
Reydem <reydem613@gmail.com>
2023-04-22 08:27:21 -0500

    Arreglando navbar

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -5,7 +5,7 @@ import logo_boomslag from 'assets/img/boomslag-black.png'
 import loading_dots from 'assets/img/loading-dots.gif'
 function Navbar() {
     return (
-        <nav className='w-full py-6 top-0 transition duration-300 ease-in-out z-40 fixed'>
+        <nav id='navbar' className='w-full py-6 top-0 transition duration-300 ease-in-out z-50 fixed'>
             <div className="px-4 sm:px-6">
                 <div className="-ml-4 -mt-2 hidden lg:flex flex-wrap items-center justify-between sm:flex-nowrap md:px-14 px-2">
                     <Link to='/' className="ml-4 mt-2">

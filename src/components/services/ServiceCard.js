a46da0e6a82f75167ce38f11bb1f8d154550f67f
Reydem <reydem613@gmail.com>
2023-04-23 10:34:18 -0500

    Movimiendo al ServiceCard

--- src/components/services/ServiceCard.js
+++ src/components/services/ServiceCard.js
@@ -1,6 +1,6 @@
 function ServiceCard({}) {
     return (
-        <div>
+        <div className="w-full relative p-8 h-96 lg:h-[200pm] bg-white hover:-translate-y-1 transition duration-300 ease-in-out">
             ServiceCard
         </div>
     )

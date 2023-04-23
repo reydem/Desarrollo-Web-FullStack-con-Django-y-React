20b9f7ba98c1f085be6238ec1d1aa1cc38fd6c9c
Reydem <reydem613@gmail.com>
2023-04-23 14:56:21 -0500

    Agregado posts_design

--- src/components/services/ServicesList.js
+++ src/components/services/ServicesList.js
@@ -3,7 +3,7 @@ import ServiceCard from "./ServiceCard"
 
 function ServicesList({posts, section_title}){
     return(
-      <div className="relative bg-gray-50 px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
+      <div className="relative bg-gray-50 px-4 pb-20 sm:px-6 lg:px-8 lg:pb-28">
         <div className="absolute inset-0">
           <div className="h-1/3 bg-white sm:h-2/3" />
         </div>

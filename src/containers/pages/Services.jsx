c9d6683b795800632642ca413d8d57c1e3fd5e89
Reydem <reydem613@gmail.com>
2023-04-23 09:44:46 -0500

    Logrando monstar ServicesList

--- src/containers/pages/Services.jsx
+++ src/containers/pages/Services.jsx
@@ -2,6 +2,7 @@ import Footer from "components/navigation/Footer"
 import Navbar from "components/navigation/Navbar"
 import Layout from "hocs/layouts/Layout"
 import Header from "components/services/Header"
+import ServicesList from "components/services/ServicesList"
 
 function Services(){
     return(
@@ -9,6 +10,7 @@ function Services(){
             <Navbar/>
             <div data-scroll-section className="pt-28">
             <Header/>
+            <ServicesList section_title={'Software and Product Development'}/>
             </div>
             <Footer/>
         </Layout>

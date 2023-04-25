379d5eb3caaa370035c99184426c317421a63dd7
Reydem <reydem613@gmail.com>
2023-04-25 16:06:28 -0500

    Agreado Helmet

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import store from './store';
 import { Provider } from 'react-redux';
 import Error404 from 'containers/errors/Error404';
 import Home from 'containers/pages/Home';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 
 
@@ -19,23 +20,48 @@ import Contact from 'containers/pages/Contact';
 // import { AnimatePresence } from 'framer-motion'
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          {/* Error Display */}
-          <Route path="*" element={<Error404 />} />
+    <HelmetProvider>
+      <Helmet>
+        <title>Murkiva | Software Agency</title>
+        <meta name="description" content="Agencia de software y marketing digital. Servicios de creacion de pagina web y desarrollo de aplicaciones." />
+        <meta name="keywords" content='agencia de software, agencia de marketing, creacion de pagina web' />
+        <meta name="robots" content='all' />
+        <link rel="canonical" href="https://www.murkiva.com/" />
+        <meta name="author" content='Murkiva' />
+        <meta name="publisher" content='Murkiva' />
 
-          {/* Home Display */}
-          <Route path="/" element={<Home />} />
-          <Route path="/casos" element={<Cases />} />
-          <Route path="/servicios" element={<Services />} />
-          <Route path="/nosotros" element={<About />} />
-          <Route path="/carreras" element={<Careers />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contacto" element={<Contact />} />
-        </Routes>
-      </Router>
-    </Provider>
+        {/* Social Media Tags */}
+        <meta property="og:title" content='Murkiva | Software Agency' />
+        <meta property="og:description" content='Agencia de software y marketing digital. Servicios de creacion de pagina web y desarrollo de aplicaciones.' />
+        <meta property="og:url" content="https://www.murkiva.com/" />
+        <meta property="og:image" content='https://bafybeicwrhxloesdlojn3bxyjqnxgsagtd4sl53a7t4cn4vfe2abmybzua.ipfs.w3s.link/lightbnuilbg.jpg' />
+
+        <meta name="twitter:title" content='Murkiva | Software Agency' />
+        <meta
+          name="twitter:description"
+          content='Agencia de software y marketing digital. Servicios de creacion de pagina web y desarrollo de aplicaciones.'
+        />
+        <meta name="twitter:image" content='https://bafybeicwrhxloesdlojn3bxyjqnxgsagtd4sl53a7t4cn4vfe2abmybzua.ipfs.w3s.link/lightbnuilbg.jpg' />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Helmet>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            {/* Error Display */}
+            <Route path="*" element={<Error404 />} />
+
+            {/* Home Display */}
+            <Route path="/" element={<Home />} />
+            <Route path="/casos" element={<Cases />} />
+            <Route path="/servicios" element={<Services />} />
+            <Route path="/nosotros" element={<About />} />
+            <Route path="/carreras" element={<Careers />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contacto" element={<Contact />} />
+          </Routes>
+        </Router>
+      </Provider>
+    </HelmetProvider>
   );
 }
 export default App;

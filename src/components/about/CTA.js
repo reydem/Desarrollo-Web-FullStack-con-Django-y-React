09e1c8edb5dce1e05cf1da95e2546ec3499b256f
Reydem <reydem613@gmail.com>
2023-04-25 10:44:49 -0500

    Agregado agreglado error CTA

--- src/components/about/CTA.js
+++ src/components/about/CTA.js
@@ -1,5 +1,6 @@
 export default function CTA() {
-    <div className="bg-gray-50">
+    return (
+        <div className="bg-gray-50">
         <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             <span className="block">Ready to dive in?</span>
@@ -25,7 +26,8 @@ export default function CTA() {
           </div>
         </div>
       </div>
+    )
+
     
   }
-  
   
